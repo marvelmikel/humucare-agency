73c636d9a33079593ac44bf96eadd19dee2a18f4
Ejeh Micheal Marvelous <49599133+marvelmikel@users.noreply.github.com>
2024-08-11 11:58:49 +0100

    update dbs check

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -238,8 +238,11 @@
                     <div class="dot"></div>
                 </div>
             </div>
+            
         </div>
+
         
+
 
 
     </div>
@@ -285,67 +288,5 @@
     </div>
 </section>
 
- <!-- Modal1 -->
- <div class="modal fade custom--modal" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
- aria-hidden="true">
- <div class="modal-dialog">
-     <div class="modal-content">
-         <div id="smart-button-container">
-             <div style="text-align: center;">
-                 <div id="paypal-button-container"></div>
-             </div>
-         </div>
-         <script
-             src="https://www.paypal.com/sdk/js?client-id=AYgUPAKrnn3sQqrOWg8HEayjK_Li5KzAUrSfAO9LP4QwjIIo-IgXE2dOyi0h4KQ-rJrD37Qx13x0FTsW&enable-funding=venmo&currency=GBP"
-             data-sdk-integration-source="button-factory"></script>
-         <script>
-             function initPayPalButton() {
-                 paypal.Buttons({
-                     style: {
-                         shape: 'rect',
-                         color: 'gold',
-                         layout: 'vertical',
-                         label: 'paypal',
 
-                     },
-
-                     createOrder: function(data, actions) {
-                         return actions.order.create({
-                             purchase_units: [{
-                                 "description": "DBS Check fee\n£ 100",
-                                 "amount": {
-                                     "currency_code": "GBP",
-                                     "value": 1
-                                 }
-                             }]
-                         });
-                     },
-
-                     onApprove: function(data, actions) {
-                         return actions.order.capture().then(function(orderData) {
-
-                             // Full available details
-                             console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
-
-                             // Show a success message within this page, e.g.
-                             const element = document.getElementById('paypal-button-container');
-                             element.innerHTML = '';
-                             element.innerHTML = '<h3>Thank you for your payment!</h3>';
-
-                             // Or go to another URL:  actions.redirect('thank_you.html');
-
-                         });
-                     },
-
-                     onError: function(err) {
-                         console.log(err);
-                     }
-                 }).render('#paypal-button-container');
-             }
-             initPayPalButton();
-         </script>
-
-     </div>
- </div>
-</div>
 @endsection
